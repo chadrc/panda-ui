3b6de119a82b00b27bb1e78ef96fa4e358f1e4d9
Chad Collins <chadrcollins91@gmail.com>
2020-10-11 16:14:33 -0400

    removed comment

--- player/PandaUIPlayer.lua
+++ player/PandaUIPlayer.lua
@@ -5,14 +5,10 @@ function PandaUIPlayer:Initialize()
         height = PandaUICore:val(150),
         layout = {direction = "horizontal"}
     }, {
+        {name = "PlayerHealth", backgroundColor = {r = 0, g = 1, b = 0, a = 1}},
         {
-            name = "PlayerHealth",
-            backgroundColor = {r = 0, g = 1, b = 0, a = 1}
-        }, {
             name = "PrimaryResource",
             backgroundColor = {r = 0, g = 0, b = 1, a = 1}
         }
     });
-
-    -- print(self.root.playerHealth:GetName());
 end
