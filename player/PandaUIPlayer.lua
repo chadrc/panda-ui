f1a3d94ff20c5d277573337c34bffe57b07f72c4
Chad Collins <chadrcollins91@gmail.com>
2020-10-11 16:12:49 -0400

    Removed commented code

--- player/PandaUIPlayer.lua
+++ player/PandaUIPlayer.lua
@@ -4,19 +4,13 @@ function PandaUIPlayer:Initialize()
     self.root = PandaUICore:CreateFrame("PlayerBars", {
         height = PandaUICore:val(150),
         layout = {direction = "horizontal"}
-        -- backgroundColor = {r = 0, g = 0, b = 1, a = 1}
     }, {
         {
             name = "PlayerHealth",
-            -- key = "playerHealth",
-            -- width = PandaUICore:pct(50),
             backgroundColor = {r = 0, g = 1, b = 0, a = 1}
         }, {
             name = "PrimaryResource",
-            -- key = "priamryResource",
-            -- width = PandaUICore:pct(50),
             backgroundColor = {r = 0, g = 0, b = 1, a = 1}
-            -- anchor = PandaUICore:anchor("BOTTOMRIGHT")
         }
     });
 
