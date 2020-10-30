6e7ed0072afec1e3024b89e51f0036cb4d0c9033
Chad Collins <chadrcollins91@gmail.com>
2020-10-30 14:03:55 -0400

    Now showing buff icon

--- player/PandaUIPlayerBuffs.lua
+++ player/PandaUIPlayerBuffs.lua
@@ -3,15 +3,42 @@ local CellHeight = 40;
 local CellPadding = 5;
 local Rows = 4;
 
-local function MakeGrid(name, count, anchor)
+local function MakeGrid(name, maxCount, anchor, filter)
     local items = {};
-    for i = 1, count do
+    for i = 1, maxCount do
         table.insert(items, {
-            name = "Buff" .. i,
+            name = name .. i,
             height = PandaUICore:val(CellHeight),
-            width = PandaUICore:val(CellWidth),
-            backgroundColor = {r = 0.02 * i, g = 0, b = 0}
+            width = PandaUICore:val(CellWidth)
+            -- backgroundColor = {r = 0.02 * i, g = 0, b = 0}
         })
+    end
+
+    local function Update(frame)
+        local index = 1;
+        AuraUtil.ForEachAura("player", filter, maxCount, function(...)
+            local _, buffTexture, count, debuffType, duration, expirationTime,
+                  _, _, _, _, _, _, _, _, timeMod = ...;
+            local buffFrame = frame.childFrames[index];
+            buffFrame.details.hidden = false;
+
+            local texture = buffFrame:CreateTexture(
+                                buffFrame:GetName() .. "Texture");
+            texture:SetTexture(buffTexture);
+            texture:SetSize(buffFrame:GetWidth(), buffFrame:GetHeight());
+            texture:SetPoint("CENTER");
+
+            buffFrame:UpdateStyles();
+
+            index = index + 1;
+            return index > maxCount;
+        end);
+
+        -- hide remaining frames
+        for i = index, maxCount do
+            frame.childFrames[i].details.hidden = true;
+            frame.childFrames[i]:UpdateStyles();
+        end
     end
 
     local grid = {
@@ -27,6 +54,14 @@ local function MakeGrid(name, count, anchor)
             cellHeight = CellHeight,
             cellPadding = CellPadding,
             start = anchor
+        },
+        events = {
+            UNIT_AURA = function(frame, unit)
+                if unit == "player" then Update(frame) end
+            end,
+            PLAYER_SPECIALIZATION_CHANGED = Update,
+            PLAYER_ENTERING_WORLD = Update,
+            GROUP_ROSTER_UPDATE = Update
         }
     };
     return grid;
@@ -37,9 +72,9 @@ function PandaUIPlayer:BuffsHeight()
 end
 
 function PandaUIPlayer:PlayerBuffs()
-    return MakeGrid("Buffs", BUFF_MAX_DISPLAY, "TOPRIGHT");
+    return MakeGrid("Buffs", BUFF_MAX_DISPLAY, "TOPRIGHT", "HELPFUL");
 end
 
 function PandaUIPlayer:PlayerDebuffs()
-    return MakeGrid("Debuggs", DEBUFF_MAX_DISPLAY, "TOPLEFT");
+    return MakeGrid("Debuggs", DEBUFF_MAX_DISPLAY, "TOPLEFT", "HARMFUL");
 end
