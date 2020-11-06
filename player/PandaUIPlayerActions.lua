ce0a100ad87b579178cfac188bb68197efe07245
Chad Collins <chadrcollins91@gmail.com>
2020-11-06 14:35:21 -0500

    Made action grid mixin

--- player/PandaUIPlayerActions.lua
+++ player/PandaUIPlayerActions.lua
@@ -82,6 +82,125 @@ function ActionButtonMixin:Setup()
   )
 end
 
+local ActionGridMixin = {}
+
+function ActionGridMixin:GetActionOffset()
+  local indexOffset = BarDetails[self.bar].offset
+
+  -- check for bonus offset
+  -- only main bar is changed to bonus
+  local bonus = GetBonusBarOffset()
+  local page = GetActionBarPage()
+  -- if displaying main bar and not on main page, override bonus bar
+  -- else use bonus if active
+  if indexOffset == 0 then
+    if page > 1 then
+      indexOffset = (page - 1) * NUM_ACTIONBAR_BUTTONS
+    elseif bonus > 0 then
+      indexOffset =
+        (NUM_ACTIONBAR_PAGES + bonus - 1) * NUM_ACTIONBAR_BUTTONS
+    end
+  end
+
+  return indexOffset
+end
+
+function ActionGridMixin:UpdateActionButtons()
+  local indexOffset = self:GetActionOffset()
+  for i, childFrame in ipairs(self.childFrames) do
+    local actionIndex = i + indexOffset
+    local usable = IsUsableAction(actionIndex)
+    local hasTarget = UnitExists("target")
+
+    local valid = IsActionInRange(actionIndex)
+    local checksRange = (valid ~= nil)
+    local inRange = checksRange and valid
+
+    if usable and (not checksRange or inRange) or not hasTarget then
+      childFrame.refs.icon.details.alpha = 1.0
+    else
+      childFrame.refs.icon.details.alpha = .25
+    end
+
+    childFrame.refs.icon:UpdateStyles()
+  end
+end
+
+function ActionGridMixin:SetupActionButtons()
+  local indexOffset = self:GetActionOffset()
+  for i, childFrame in ipairs(self.childFrames) do
+    local actionIndex = i + indexOffset
+    childFrame.actionIndex = actionIndex
+
+    local texture = GetActionTexture(actionIndex)
+    childFrame.refs.icon.details.texture.file = texture
+    childFrame.refs.icon:UpdateStyles()
+  end
+
+  self:UpdateActionButtons()
+end
+
+function ActionGridMixin:CheckRangeChecker()
+  if UnitExists("target") and not self.hasTarget then
+    -- new target register update event to watch range changes
+    self.hasTarget = true
+    self:SetScript(
+      "OnUpdate",
+      function(frame)
+        UpdateActionButtons(frame)
+      end
+    )
+  elseif not UnitExists("target") then
+    -- de-targeted
+    self.hasTarget = false
+    self:SetScript("OnUpdate", nil)
+  end
+  self:UpdateActionButtons()
+end
+
+function ActionGridMixin:UpdateModifiers(key, pressed)
+  local function CheckMod(name, index)
+    if string.find(key, name) then
+      if pressed == 1 then
+        self.mods[index] = string.lower(name)
+      else
+        self.mods[index] = ""
+      end
+    end
+  end
+
+  -- update state with pressed/relased
+  CheckMod("ALT", 1)
+  CheckMod("CTRL", 2)
+  CheckMod("SHIFT", 3)
+
+  -- use state to determine current mod combination and bar to use
+  local downMods = {}
+  for _, v in pairs(self.mods) do
+    if v ~= "" then
+      table.insert(downMods, v)
+    end
+  end
+  local modStr = table.concat(downMods, "-")
+
+  for i, details in ipairs(BarDetails) do
+    if modStr == details.mods then
+      self.bar = i
+    end
+  end
+
+  self:SetupActionButtons()
+end
+
+function ActionGridMixin:Setup()
+  self.bar = 1
+  self.mods = {
+    "", -- alt
+    "", -- ctrl
+    "" -- shift
+  }
+end
+
 function PandaUIPlayer:Actions()
   local buttons = {}
   for i = 1, 12 do
@@ -109,68 +228,13 @@ function PandaUIPlayer:Actions()
     )
   end
 
-  local function GetActionOffset(frame)
-    local indexOffset = BarDetails[frame.bar].offset
-
-    -- check for bonus offset
-    -- only main bar is changed to bonus
-    local bonus = GetBonusBarOffset()
-    local page = GetActionBarPage()
-    -- if displaying main bar and not on main page, override bonus bar
-    -- else use bonus if active
-    if indexOffset == 0 then
-      if page > 1 then
-        indexOffset = (page - 1) * NUM_ACTIONBAR_BUTTONS
-      elseif bonus > 0 then
-        indexOffset =
-          (NUM_ACTIONBAR_PAGES + bonus - 1) * NUM_ACTIONBAR_BUTTONS
-      end
-    end
-
-    return indexOffset
-  end
-
-  local function UpdateActionButtons(frame)
-    local indexOffset = GetActionOffset(frame)
-    for i, childFrame in ipairs(frame.childFrames) do
-      local actionIndex = i + indexOffset
-      local usable = IsUsableAction(actionIndex)
-      local hasTarget = UnitExists("target")
-
-      local valid = IsActionInRange(actionIndex)
-      local checksRange = (valid ~= nil)
-      local inRange = checksRange and valid
-
-      if usable and (not checksRange or inRange) or not hasTarget then
-        childFrame.refs.icon.details.alpha = 1.0
-      else
-        childFrame.refs.icon.details.alpha = .25
-      end
-
-      childFrame.refs.icon:UpdateStyles()
-    end
-  end
-
-  local function SetupActionButtons(frame)
-    local indexOffset = GetActionOffset(frame)
-    for i, childFrame in ipairs(frame.childFrames) do
-      local actionIndex = i + indexOffset
-      childFrame.actionIndex = actionIndex
-
-      local texture = GetActionTexture(actionIndex)
-      childFrame.refs.icon.details.texture.file = texture
-      childFrame.refs.icon:UpdateStyles()
-    end
-
-    UpdateActionButtons(frame)
-  end
-
   return {
     name = "Actions",
     -- backgroundColor = {r = 1.0, g = 0, b = 0},
     children = {
       {
         name = "Buttons",
+        mixin = ActionGridMixin,
         anchor = PandaUICore:anchor("TOP"),
         width = PandaUICore:val(GridWidth),
         childLayout = {
@@ -182,71 +246,16 @@ function PandaUIPlayer:Actions()
           start = "TOPLEFT"
         },
         children = buttons,
-        init = function(frame)
-          frame.bar = 1
-          frame.mods = {
-            "", -- alt
-            "", -- ctrl
-            "" -- shift
-          }
-        end,
+        init = "Setup",
         events = {
-          PLAYER_ENTERING_WORLD = SetupActionButtons,
-          UPDATE_SHAPESHIFT_FORM = SetupActionButtons,
-          PLAYER_SPECIALIZATION_CHANGED = SetupActionButtons,
-          ACTIONBAR_UPDATE_STATE = UpdateActionButtons,
-          ACTIONBAR_PAGE_CHANGED = SetupActionButtons,
-          ACTIONBAR_SLOT_CHANGED = SetupActionButtons,
-          PLAYER_TARGET_CHANGED = function(frame)
-            if UnitExists("target") and not frame.hasTarget then
-              -- new target register update event to watch range changes
-              frame.hasTarget = true
-              frame:SetScript(
-                "OnUpdate",
-                function(frame)
-                  UpdateActionButtons(frame)
-                end
-              )
-            elseif not UnitExists("target") then
-              -- de-targeted
-              frame.hasTarget = false
-              frame:SetScript("OnUpdate", nil)
-            end
-            UpdateActionButtons(frame)
-          end,
-          MODIFIER_STATE_CHANGED = function(frame, key, pressed)
-            local function CheckMod(name, index)
-              if string.find(key, name) then
-                if pressed == 1 then
-                  frame.mods[index] = string.lower(name)
-                else
-                  frame.mods[index] = ""
-                end
-              end
-            end
-
-            -- update state with pressed/relased
-            CheckMod("ALT", 1)
-            CheckMod("CTRL", 2)
-            CheckMod("SHIFT", 3)
-
-            -- use state to determine current mod combination and bar to use
-            local downMods = {}
-            for _, v in pairs(frame.mods) do
-              if v ~= "" then
-                table.insert(downMods, v)
-              end
-            end
-            local modStr = table.concat(downMods, "-")
-
-            for i, details in ipairs(BarDetails) do
-              if modStr == details.mods then
-                frame.bar = i
-              end
-            end
-
-            SetupActionButtons(frame)
-          end
+          PLAYER_ENTERING_WORLD = "SetupActionButtons",
+          UPDATE_SHAPESHIFT_FORM = "SetupActionButtons",
+          PLAYER_SPECIALIZATION_CHANGED = "SetupActionButtons",
+          ACTIONBAR_UPDATE_STATE = "UpdateActionButtons",
+          ACTIONBAR_PAGE_CHANGED = "SetupActionButtons",
+          ACTIONBAR_SLOT_CHANGED = "SetupActionButtons",
+          PLAYER_TARGET_CHANGED = "CheckRangeChecker",
+          MODIFIER_STATE_CHANGED = "UpdateModifiers"
         }
       }
     }
