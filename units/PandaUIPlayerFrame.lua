a227767819da1b063055d8936bab2c474d58bf58
Chad Collins <chadrcollins91@gmail.com>
2020-11-08 09:40:03 -0500

    Moved player and target frame to own files

---
+++ units/PandaUIPlayerFrame.lua
@@ -0,0 +1,33 @@
+function PandaUIUnits:PlayerFrame(vars, charVars)
+  local menuFunc = function()
+    ToggleDropDownMenu(1, nil, PlayerFrameDropDown, "cursor", 0, 0)
+  end
+  local details =
+    self:UnitFrame("player", menuFunc, "HELPFUL RAID", "HARMFUL RAID")
+
+  PandaUICore:SetMovable(
+    details,
+    {
+      point = "CENTER",
+      relativePoint = "CENTER",
+      xOfs = 0,
+      yOfs = -200
+    },
+    vars.Player,
+    "position"
+  )
+
+  details.events.PLAYER_LEVEL_UP = function(frame, level)
+    frame.level = level
+    frame:Update()
+  end
+  details.events.PLAYER_LEVEL_CHANGED = function(
+    frame,
+    oldLevel,
+    newLevel)
+    frame.level = newLevel
+    frame:Update()
+  end
+
+  return details
+end
---
+++ units/PandaUITargetFrame.lua
@@ -0,0 +1,69 @@
+function PandaUIUnits:TargetFrame(vars, charVars)
+  local dropdown = TargetFrameDropDown
+  local menuFunc = TargetFrameDropDown_Initialize
+  UIDropDownMenu_SetInitializeFunction(dropdown, menuFunc)
+  UIDropDownMenu_SetDisplayMode(dropdown, "MENU")
+
+  local showmenu = function()
+    ToggleDropDownMenu(1, nil, dropdown, "cursor", 0, 0)
+  end
+  local details =
+    self:UnitFrame("target", showmenu, "HELPFUL", "HARMFUL PLAYER")
+  details.hidden = true
+
+  local function SetupTarget(frame)
+    local info = PandaUIUnits:GetUnitInfo("target")
+    local playerInCombat = InCombatLockdown()
+
+    if info.exists then
+      if not playerInCombat then
+        frame:Show()
+      end
+
+      if info.isFriend then
+        frame.backgroundColor = {r = 0, g = .5, b = 0, a = .4}
+      elseif info.isEnemy then
+        frame.backgroundColor = {r = .5, g = 0, b = 0, a = .4}
+      else
+        -- let frame use its default
+        frame.backgroundColor = nil
+      end
+    elseif not playerInCombat then
+      frame.details.hidden = true
+      frame:Hide()
+    end
+
+    frame:Setup()
+  end
+
+  details.events.PLAYER_ENTERING_WORLD = SetupTarget
+  details.events.PLAYER_TARGET_CHANGED = SetupTarget
+  details.events.PLAYER_REGEN_DISABLED = function(frame)
+    -- try to catch before combat and show target frame
+    if not UnitExists("target") and not InCombatLockdown() then
+      frame.details.hidden = false
+      frame:UpdateStyles()
+    end
+  end
+
+  details.events.PLAYER_REGEN_ENABLED = function(frame)
+    if not UnitExists("target") and not InCombatLockdown() then
+      frame.details.hidden = true
+      frame:UpdateStyles()
+    end
+  end
+
+  PandaUICore:SetMovable(
+    details,
+    {
+      point = "CENTER",
+      relativePoint = "CENTER",
+      xOfs = 0,
+      yOfs = 200
+    },
+    vars.Target,
+    "position"
+  )
+
+  return details
+end
